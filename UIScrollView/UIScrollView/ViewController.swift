18d2bf8e9127ff886ded29ed95a9aede357ff97c
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-04-29 14:56:45 +0900

    1ページ分のビューに関する実装

--- UIScrollView/UIScrollView/ViewController.swift
+++ UIScrollView/UIScrollView/ViewController.swift
@@ -34,6 +34,25 @@ class ViewController: UIViewController {
             Photo(imageName:"photo4", title:"夏の垣根")
             ]
     }
+    
+    // 写真とタイトルがある１ページ分のビューを作る
+    func createPage(viewRect:CGRect, imageSize:CGSize, item:Photo) -> UIView {
+        let pageView = UIView(frame: viewRect)
+        // 写真ビューを作ってイメージを設定する
+        let photoView = UIImageView()
+        let left = (pageView.frame.width - imageSize.width)/2
+        photoView.frame = CGRect(x:left, y:10, width:imageSize.width, height: imageSize.height)
+        photoView.contentMode = .scaleToFill
+        photoView.image = UIImage(named: item.imageName)
+        // ラベルを作って写真タイトルを設定する
+        let titleFrame = CGRect(x:left, y:photoView.frame.maxY+10, width:200, height:21)
+        let titleLabel = UILabel(frame: titleFrame)
+        titleLabel.text = item.title
+        // 写真とタイトルとページビューに追加する
+        pageView.addSubview(photoView)
+        pageView.addSubview(titleLabel)
+        return pageView
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
